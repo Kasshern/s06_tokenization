eef6736b8f2d1cc95c3d12d18d1e3f211f12ceaf
Kasshern <1400536468@qq.com>
2021-10-30 18:03:31 -0500

    setup chaisetup.js file, altered MyToken.test and MyTokenSale.test to make use of chai.config

---
+++ test/MyTokenSale.test.js
@@ -0,0 +1,30 @@
+const Token = artifacts.require("MyToken");
+const TokenSale = artifacts.require("MyTokenSale");
+
+const chai = require("./chaisetup.js");
+const BN = web3.utils.BN;
+const expect = chai.expect;
+contract("TokenSale", async function(accounts) {
+    const [initialHolder, recepient, anotherAccount] = accounts;
+
+    it("there shouldn't be any coins in my account", async () => {
+        let instance = await Token.deployed();
+        return expect(instance.balanceOf.call(initialHolder)).to.eventually.be.a.bignumber.equal(new BN(0));
+    })
+
+    it("all coins should be in the tokensale smart contract", async () => {
+        let instance = await Token.deployed();
+        let balance = await instance.balanceOf.call(TokenSale.address);
+        let totalSupply = await instance.totalSupply.call();
+        return expect(balance).to.be.a.bignumber.equal(totalSupply);
+    })
+
+    it("should be possible to buy one token by simple sending ether to smart contract", async () => {
+        let tokenInstance = await Token.deployed();
+        let tokenSaleInstance = await TokenSale.deployed();
+        let balanceBeforeAccount = await tokenInstance.balanceOf.call(recepient);
+
+        await expect(tokenSaleInstance.sendTransaction({from: recepient, value: web3.utils.toWei("1", "wei")})).to.be.fulfilled;
+        return expect(balanceBeforeAccount + 1).to.be.a.bignumber.equal(await tokenInstance.balanceOf.call(recepient)) ;
+    })
+})
